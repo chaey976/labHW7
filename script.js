a499cef98a8311dc62570d85e5e1da251002f4ff
chaey976 <chaey976@newschool.edu>
2021-03-13 00:31:48 +0900

    upload

---
+++ script.js
@@ -0,0 +1,79 @@
+var imageone = document.getElementById('item1');
+
+    imageone.addEventListener('click', function(){
+        changeText();
+    });
+
+    function changeText(){
+        document.getElementById('bio').innerHTML = 'I am Korean! Just like BTS.';
+    }
+
+var imagetwo = document.getElementById('item2');
+
+    imagetwo.addEventListener('click', function(){
+        changeTextTwo();
+    });
+
+    function changeTextTwo(){
+        document.getElementById('bio').innerHTML = 'I have two dogs; a mini-Austrailian shepherd named Hanzo, and a maltese named Leo!';
+    }
+
+var imagethree = document.getElementById('item3');
+
+    imagethree.addEventListener('click', function(){
+        changeTextThree();
+    });
+
+    function changeTextThree(){
+        document.getElementById('bio').innerHTML = 'My zodiac sign is gemini.';
+    }
+
+var imagefour = document.getElementById('item4');
+
+    imagefour.addEventListener('click', function(){
+        changeTextFour();
+    });
+
+    function changeTextFour(){
+        document.getElementById('bio').innerHTML = 'I HATE studying.. That is why I applied to an art school.';
+    }
+
+var imageeight = document.getElementById('item8');
+
+    imageeight.addEventListener('click', function(){
+        changeTextEight();
+    });
+
+    function changeTextEight(){
+        document.getElementById('bio').innerHTML = 'Although my clothes are mostly black, white, and gray, I like to keep my room pink!';
+    }
+
+var imagefive = document.getElementById('item5');
+
+    imagefive.addEventListener('click', function(){
+        changeTextFive();
+    });
+
+    function changeTextFive(){
+        document.getElementById('bio').innerHTML = 'After I graduate, I am planning on getting a cat..';
+    }
+
+var imagesix = document.getElementById('item6');
+
+    imagesix.addEventListener('click', function(){
+        changeTextSix();
+    });
+
+    function changeTextSix(){
+        document.getElementById('bio').innerHTML = 'a cute bird..';
+    }
+
+var imageseven = document.getElementById('item7');
+
+    imageseven.addEventListener('click', function(){
+        changeTextSeven();
+    });
+
+    function changeTextSeven(){
+        document.getElementById('bio').innerHTML = 'and also a bearded dragon!';
+    }
